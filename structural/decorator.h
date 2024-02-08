c712217bb9bc30ab222765d7b25f6bfc22b06920
김태영 <7d6h8t@gmail.com>
2024-02-08 11:35:41 +0900

    update decorator

--- structural/decorator.h
+++ structural/decorator.h
@@ -1,10 +1,15 @@
 /**
  * decorator is a structural design pattern.
+ * 1. 기본 기능 정의된 인터페이스 Component
+ * 2. Component 를 구현한 기본 객체 Concrete Component 추가
+ * 3. Component 의 기능을 하위 객체에 위임하기 위한 Decorator 추가
+ * 4. Decorator 를 상속받아 실제 추가 기능을 Concrete Decorator 에 구현
+ * 기존 코드를 수정x + 추가기능 확장 가능 (여러 조합을 통해)
  */
 #include <iostream>
 #include <memory>
 
-// Component
+// Component : 기본 기능 정의
 class Weapon {
  public:
   virtual ~Weapon() = default;
@@ -14,13 +19,14 @@ class Weapon {
   Weapon() = default;
 };
 
-// Concrete Component
+// Concrete Component : 기본 기능을 구현한 객체 정의 (여기에 Decorator 들이
+// 추가로 붙게됨)
 class BaseWeapon : public Weapon {
  public:
   void Fire() const override { std::cout << "fire" << std::endl; }
 };
 
-// Decorator
+// Decorator : 추가될 기능 정의
 class Accessory : public Weapon {
  public:
   Accessory(std::unique_ptr<Weapon>&& rifle) : rifle_(std::move(rifle)) {}
@@ -31,7 +37,7 @@ class Accessory : public Weapon {
   std::unique_ptr<Weapon> rifle_;
 };
 
-// Concrete Decorator
+// Concrete Decorator : 실제로 추가될 기능 구현
 class Generade : public Accessory {
  public:
   Generade(std::unique_ptr<Weapon>&& rifle) : Accessory(std::move(rifle)) {}
@@ -44,7 +50,7 @@ class Generade : public Accessory {
   void GeneradeFire() const { std::cout << "generade fire" << std::endl; }
 };
 
-// Concrete Decorator
+// Concrete Decorator : 실제로 추가될 기능 구현
 class Scoped : public Accessory {
  public:
   Scoped(std::unique_ptr<Weapon>&& rifle) : Accessory(std::move(rifle)) {}
@@ -57,7 +63,7 @@ class Scoped : public Accessory {
   void Aiming() const { std::cout << "Aiming..." << std::endl; }
 };
 
-// Concrete Decorator
+// Concrete Decorator : 실제로 추가될 기능 구현
 class Buttstock : public Accessory {
  public:
   Buttstock(std::unique_ptr<Weapon>&& rifle) : Accessory(std::move(rifle)) {}
