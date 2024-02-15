17d3b02f073b07ce284e54f33dc460fc4d6b532b
김태영 <7d6h8t@gmail.com>
2024-02-15 14:12:06 +0900

    modify flyweight

--- structural/flyweight.h
+++ structural/flyweight.h
@@ -1,11 +1,18 @@
 /**
  * fryweight is a structural design pattern.
+ * 1. 공유 가능한 (변하지 않는) 데이터 ConcreteFlyweight.
+ * 2. 공유 불가능한 데이터 ConcreteFlyweight(unshared) 로 분리하여
+ * 3. ConcreteFlyweight 를 관리 (생성, 캐시) 하는 팩토리 FlyweightFactory 구현.
+ * FlyweightFactory 로 데이터 경량화 (공유 데이터 캐시)를 관리함.
+ * 메모리 최적화를 위해 상태가 변하지 않는 데이터를 재사용(공유) 할때 사용함.
+ * (RAM 이 부족한 경우가 아니라면 거의 사용하지 않는 패턴임.)
  */
 #include <iostream>
 #include <memory>
 #include <string>
 #include <unordered_map>
 
+// ConcreteFlyweight
 class TreeSharedState {
  public:
   TreeSharedState(const std::string& type, const std::string& mesh,
@@ -22,6 +29,7 @@ class TreeSharedState {
   std::string texture_;
 };
 
+// ConcreteFlyweight (unshared)
 class Tree {
  public:
   Tree(const TreeSharedState& shared_state, const double x, const double y)
@@ -36,6 +44,7 @@ class Tree {
   double y_;
 };
 
+// FlyweightFactory
 class Factory final {
  public:
   static Factory& GetInstance() {
@@ -47,20 +56,22 @@ class Factory final {
     if (cache_.find(key) != cache_.end())
       return *cache_.at(key);
     else {
-      cache_[key] = new TreeSharedState(key, "mesh", "texture");
+      cache_[key] = std::make_unique<TreeSharedState>(key, "mesh", "texture");
       return *cache_[key];
     }
   }
 
  private:
-  static std::unordered_map<std::string, TreeSharedState*> cache_;
+  static std::unordered_map<std::string, std::unique_ptr<TreeSharedState>>
+      cache_;
 };
 
-std::unordered_map<std::string, TreeSharedState*> Factory::cache_;
+std::unordered_map<std::string, std::unique_ptr<TreeSharedState>>
+    Factory::cache_;
 
+/*
 void CreateTree(const std::string& type, const double x, const double y) {
-  TreeSharedState tree_state = Factory::GetInstance().GetSharedState(type);
-  Tree tree(tree_state, x, y);
+  Tree tree(Factory::GetInstance().GetSharedState(type), x, y);
 
   std::cout << "x:" << tree.GetX() << " y:" << tree.GetY() << " 위치에 " << type
             << " 나무 생성 완료" << std::endl;
@@ -77,3 +88,4 @@ int main() {
 
   return 0;
 }
+*/
