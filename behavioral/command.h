f13bdcbba058fcdc649d52a25d7d34bdf9bfe33c
7d6h8t <7d6h8t@gmail.com>
2024-02-20 01:40:40 +0900

    update command

--- behavioral/command.h
+++ behavioral/command.h
@@ -5,85 +5,62 @@
 #include <iostream>
 #include <string>
 
+// Command
 class Command {
  public:
   virtual ~Command() {}
-  virtual void Execute() const = 0;
+  virtual void Execute() = 0;
 };
 
-class SimpleCommand : public Command {
+// Recevicer
+class Light {
  public:
-  explicit SimpleCommand(const std::string& pay_load) : pay_load_(pay_load) {}
-  void Execute() const override {
-    std::cout << "SimpleCommand: See, I can do simple things like printing ("
-              << pay_load_ << ")\n";
-  }
+  void TurnOn() { std::cout << "turn on" << std::endl; }
+  void TurnOff() { std::cout << "turn off" << std::endl; }
+};
+
+// ConcreteCommand
+class TurnOnCommand : public Command {
+ public:
+  TurnOnCommand(Light* Light) : light_(Light) {}
+  void Execute() override { light_->TurnOn(); }
 
  private:
-  std::string pay_load_;
+  Light* light_;
 };
 
-class Receiver {
+// ConcreteCommand
+class TurnOffCommand : public Command {
  public:
-  void DoSomething(const std::string& a) {
-    std::cout << "Receiver: Working on (" << a << ".)\n";
-  }
-  void DoSomethingElse(const std::string& b) {
-    std::cout << "Receiver: Also working on (" << b << ".)\n";
-  }
-};
-
-class ComplexCommand : public Command {
- public:
-  ComplexCommand(Receiver* receiver, const std::string& a, const std::string& b)
-      : receiver_(receiver), a_(a), b_(b) {}
-
-  void Execute() const override {
-    std::cout << "ComplexCommand: Complex stuff should be done by a receiver "
-                 "object.\n";
-    receiver_->DoSomething(a_);
-    receiver_->DoSomethingElse(b_);
-  }
+  TurnOffCommand(Light* Light) : light_(Light) {}
+  void Execute() override { light_->TurnOff(); }
 
  private:
-  Receiver* receiver_;
-  std::string a_;
-  std::string b_;
+  Light* light_;
 };
 
-class Invoker {
+// invoker
+class RemoteControl {
  public:
-  ~Invoker() {
-    delete on_start_;
-    delete on_finish_;
-  }
-
-  void SetOnStart(Command* command) { on_start_ = command; }
-  void SetOnFinish(Command* command) { on_finish_ = command; }
-  void DoSomethingImportant() {
-    std::cout << "Invoker: Does anybody want something done before I begin?\n";
-    if (on_start_) on_start_->Execute();
-
-    std::cout << "Invoker: ...doing something really important...\n";
-    std::cout << "Invoker: Does anybody want something done after I finish?\n";
-    if (on_finish_) on_finish_->Execute();
-  }
+  void SetCommand(Command* command) { command_ = command; }
+  void PressButton() { command_->Execute(); }
 
  private:
-  Command* on_start_;
-  Command* on_finish_;
+  Command* command_;
 };
 
 int main() {
-  Invoker* invoker = new Invoker;
-  invoker->SetOnStart(new SimpleCommand("Say Hi!"));
-  Receiver* receiver = new Receiver;
-  invoker->SetOnFinish(
-      new ComplexCommand(receiver, "Send email", "Save report"));
-  invoker->DoSomethingImportant();
+  Light light_;
+  TurnOnCommand turn_on(&light_);
+  TurnOffCommand turn_off(&light_);
 
-  delete invoker;
-  delete receiver;
+  RemoteControl remote;
+  remote.SetCommand(&turn_on);
+
+  remote.PressButton();
+
+  remote.SetCommand(&turn_off);
+  remote.PressButton();
 
   return 0;
 }
