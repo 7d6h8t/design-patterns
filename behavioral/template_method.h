4b2edd055d5a33f6a3defb980b6f408f51da4c77
김태영 <7d6h8t@gmail.com>
2024-02-27 14:14:58 +0900

    update template method

--- behavioral/template_method.h
+++ behavioral/template_method.h
@@ -1,5 +1,9 @@
 /**
  * Tempate Method is a behavioral design pattern.
+ * 1. 공통 기능 구현 + 다른 부분은 추상함수로 부모 클래스 정의 AbstractClass
+ * 2. 부모 클래스를 상속받아 다른 부분으로 정의된 추상함수 구현 ConcreteClass
+ * 부모 클래스에서 처리 흐름 제어 -> 자식 클래스에서 처리 내용 구현.
+ * 동일한 코드에서 몇개의 다른 부분만 필요한 경우, 코드 중복을 위해 사용.
  */
 
 #include <iostream>
@@ -16,6 +20,7 @@ class CoffeeMaker {
     Hook();
   }
 
+ protected:
   virtual void BrewCoffeeGrounds() = 0;
   virtual void AddCondiments() = 0;
   virtual void Hook() {
@@ -48,6 +53,7 @@ class Latte : public CoffeeMaker {
   }
 };
 
+/*
 int main() {
   std::cout << "[Make Americano]" << std::endl;
   Americano americano;
@@ -57,3 +63,4 @@ int main() {
   Latte latte;
   latte.Make();
 }
+*/
