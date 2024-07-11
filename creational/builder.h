a6898bb87765de1a1393dfafc323c35e478c52e9
7d6h8t <7d6h8t@gmail.com>
2024-07-11 09:55:50 +0900

    update builder

--- creational/builder.h
+++ creational/builder.h
@@ -8,14 +8,17 @@
  */
 #include <iostream>
 #include <memory>
+#include <string>
 
 // Product : Director 가 Builder로 만들어낸 결과물.
 class Character {
  public:
-  Character(const int32_t weapon, const int32_t armor)
-      : weapon_(weapon), armor_(armor) {}
+  void SetWeapon(const int32_t weapon) { weapon_ = weapon; }
+  void SetArmor(const int32_t armor) { armor_ = armor; }
+  void SetName(const std::string& name) { name_ = name; }
 
   void ShowInformation() {
+    std::cout << "Name :" << name_ << std::endl;
     std::cout << "Weapon power:" << weapon_ << std::endl;
     std::cout << "Armor defence:" << armor_ << std::endl;
   }
@@ -23,56 +26,58 @@ class Character {
  private:
   int32_t weapon_;
   int32_t armor_;
+  std::string name_;
 };
 
 // Builder
 class CharacterBuilder {
  public:
-  virtual int32_t CreateWeapon() = 0;
-  virtual int32_t CreateArmor() = 0;
+  virtual void BuildWeapon(const int32_t weapon) = 0;
+  virtual void BuildArmor(const int32_t armor) = 0;
+  virtual void BuildName(const std::string& name) = 0;
+  virtual Character GetResult() = 0;
 };
 
 // Concrete Builder for archer
-class ArcherBuilder : public CharacterBuilder {
+class TeranBuilder : public CharacterBuilder {
  public:
-  int32_t CreateWeapon() override { return 200; }
-  int32_t CreateArmor() override { return 50; }
-};
+  void BuildWeapon(const int32_t weapon) override {
+    character_.SetWeapon(weapon);
+  }
+  void BuildArmor(const int32_t armor) override { character_.SetArmor(armor); }
+  void BuildName(const std::string& name) override { character_.SetName(name); }
 
-// Concrete builder for warrior
-class WarriorBuilder : public CharacterBuilder {
- public:
-  int32_t CreateWeapon() override { return 50; }
-  int32_t CreateArmor() override { return 200; }
+  Character GetResult() override { return character_; }
+
+ private:
+  Character character_;
 };
 
 // Director
 class CharacterDirector {
  public:
-  void SetBuilder(std::unique_ptr<CharacterBuilder>&& builder) {
-    builder_ = std::move(builder);
+  Character CreateMarine(std::unique_ptr<CharacterBuilder>&& builder) {
+    builder->BuildWeapon(10);
+    builder->BuildArmor(5);
+    builder->BuildName("Marine");
+    return builder->GetResult();
   }
 
-  Character CreateCharacter() {
-    return {builder_->CreateWeapon(), builder_->CreateArmor()};
+  Character CreateMedic(std::unique_ptr<CharacterBuilder>&& builder) {
+    builder->BuildWeapon(0);
+    builder->BuildArmor(6);
+    builder->BuildName("Medic");
+    return builder->GetResult();
   }
-
- private:
-  std::unique_ptr<CharacterBuilder> builder_ = nullptr;
 };
 
 int main() {
-  CharacterDirector director;
+  CharacterDirector barak;
 
-  director.SetBuilder(std::make_unique<ArcherBuilder>());
+  Character marine = barak.CreateMarine(std::make_unique<TeranBuilder>());
+  Character medic = barak.CreateMedic(std::make_unique<TeranBuilder>());
 
-  Character character = director.CreateCharacter();
-  character.ShowInformation();
-
-  director.SetBuilder(std::make_unique<WarriorBuilder>());
-
-  character = director.CreateCharacter();
-  character.ShowInformation();
-
+  marine.ShowInformation();
+  medic.ShowInformation();
   return 0;
 }
